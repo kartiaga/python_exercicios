fbd58fbb9b340f13435337e9aef96b7743cc00a5
Kaique Artiaga <kaiqueartiagaluz@gmail.com>
2023-12-19 17:55:05 -0300

    Primeiros passos com o python

---
+++ ex002.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+name = input('Qual seu nome? ')
+
+print('Prazer em conhecer você ', name, '!')
+print('Prazer em te conhecer {}!'.format(name))
